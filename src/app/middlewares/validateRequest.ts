0e2d671708416b0155f8ee2c865ffd81a594b1b3
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2025-11-30 19:28:47 +0600

    Added Zod Validation Helper Function

---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodObject } from "zod";
+
+export const validateRequest = (zodSchema: ZodObject) => async (req: Request, _res: Response, next: NextFunction) => {
+    try {
+        if (req.body.data) {
+            req.body = JSON.parse(req.body.data)
+        }
+
+        req.body = await zodSchema.parseAsync(req.body)
+        next()
+    } catch (err) {
+        next(err)
+    }
+}
